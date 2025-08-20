481dd94ed91bc25470dd38ff5a542afbc701a9d0
andrae101 <cunninghamandrae34@gmail.com>
2025-08-20 18:25:16 -0500

    Update jobs.js
    Signed-off-by: andrae101 <cunninghamandrae34@gmail.com>

--- api/jobs.js
+++ api/jobs.js
@@ -1,7 +1,11 @@
 // api/jobs.js
 export default async function handler(req, res) {
-  const response = await fetch("https://remotive.com/api/remote-jobs");
-  const data = await response.json();
+  try {
+    const response = await fetch("https://remotive.com/api/remote-jobs");
+    const data = await response.json();
 
-  res.status(200).json(data);
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({ error: "Failed to fetch jobs" });
+  }
 }
